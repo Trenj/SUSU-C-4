720f5d6358a20f1e639c19cc64f55616069a87c9
Андрей Замятин <andreizamyat1n@yandex.ru>
2024-02-25 20:23:17 +0500

    Реализация самостоятельной программы

--- СМ_1/СМ_1.cpp
+++ СМ_1/СМ_1.cpp
@@ -1,8 +1,55 @@
 ﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Book
+{
+private:
+
+	// Member Variables 
+	string title;
+	string author;
+	int publicationYear;
+	string availabilitystatus;
+
+public:
+
+	// Constructor 
+	Book(string t, string a, int year, string p) : title(t), author(a), publicationYear(year), availabilitystatus(p) {}
+
+	// Member Function to display book details 
+	void displayDetails()
+	{
+		cout << "Title: " << title << endl;
+		cout << "Author: " << author << endl;
+		cout << "Publication Year: " << publicationYear << endl;
+		cout << "Availability Status: " << availabilitystatus << endl;
+	}
+
+	// Member Function to update the book's price 
+	void updateAvailabilitystatus(string newAvailabilitystatus)
+	{
+		availabilitystatus = newAvailabilitystatus;
+	}
+};
 
 int main()
 {
-    
+	
+	// Creating an object of Book class and initializing it 
+	Book myBook("Simka Time", "Pablo Banopart", 1925, "are available");
+
+	// Displaying book details 
+	cout << "Initial book details:" << endl; myBook.displayDetails();
+
+	// Updating the book's price
+	myBook.updateAvailabilitystatus("not available");
+
+	// Displaying updated book details 
+	cout << "\nUpdated book details:" << endl;
+	myBook.displayDetails();
+
+	return 0;
 }
 
 
